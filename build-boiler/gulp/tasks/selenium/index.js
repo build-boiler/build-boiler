5f6519c0ef9f3b3721e6d612caec4ab59bed4c7a
dtothefp <dtothefp@gmail.com>
2016-02-21 17:51:37 -0500

    add testing to build-boiler
    add copy task for test configs
    
    add selenium runner
    
    update postinstall runner
    
    stop clobbering
    
    update postinstall path
    
    update binary path for webpdriver
    
    add cache to travis
    
    update test path...again
    
    add the root path
    
    finally fix copy path
    
    more path fixes
    
    remove prepublish script
    
    fixup build
    
    fix github url
    
    hopefully finally correct
    
    use root path
    
    add missing dep
    
    update binary path for webdriver
    
    update selenium babel config
    
    add plus tag
    
    fix publish regex
    
    uptate publish script
    
    update publish regex
    
    bump

---
+++ build-boiler/gulp/tasks/selenium/index.js
@@ -0,0 +1,115 @@
+import {log} from 'gulp-util';
+import spawn from './spawn-process';
+import makeConfig from './make-config';
+import thunk from '../../utils/thunk';
+import run from '../../utils/run-gen';
+
+export default function(gulp, plugins, config) {
+  const {utils} = config;
+  const {logError} = utils;
+
+  return (gulpCb) => {
+    const {
+      testEnv,
+      installOpts,
+      spawnOpts,
+      spawnTunnelOpts,
+      testConfig,
+      task,
+      tunnelOnly
+    } = makeConfig({config, gulp});
+
+    function runWebdriver(cb) {
+      return spawn(testConfig, config, cb);
+    }
+
+    function logStatus(code) {
+      process.exit(code);
+      gulpCb();
+    }
+
+    /**
+     * Two possibilities
+     * a) `testEnv === 'tunnel'` Browser tests must be run on BrowserStack
+     * b) `task === 'tunnel'` the command was `gulp selenium:tunnel` for "Live" preview on BrowserStack
+     */
+    if (testEnv === 'tunnel' || task === 'tunnel') {
+      const BrowserStackTunnel = require('browserstacktunnel-wrapper');
+      /**
+       * gulp selenium:tunnel
+       * Start a Browserstack tunnel to allow using local IP's for
+       * Browserstack tests (Automate) and live viewing (Live)
+       */
+      const browserStackTunnel = new BrowserStackTunnel(spawnTunnelOpts);
+
+      browserStackTunnel.on('started', () => {
+        log(browserStackTunnel.stdoutData);
+      });
+
+      run(function *() {
+        let startTunnel = thunk(browserStackTunnel.start, browserStackTunnel);
+        try {
+          yield startTunnel();
+        } catch (err) {
+          logError({err, plugin: '[tunnel start]'});
+        }
+
+        if (tunnelOnly) {
+          log('Visit BrowserStack Live to QA: https://www.browserstack.com/start');
+        } else {
+          let cp = thunk(runWebdriver);
+          let code = yield cp();
+          let stopTunnel = thunk(browserStackTunnel.stop, browserStackTunnel);
+
+          try {
+            yield stopTunnel();
+          } catch (err) {
+            logError({err, plugin: '[tunnel stop]'});
+          }
+
+          logStatus(code);
+        }
+      });
+    } else if (testEnv === 'local') {
+      const selenium = require('selenium-standalone');
+      const install = require('./install');
+
+      run(function *() {
+        let seleniumInstall = thunk(install);
+        try {
+          yield seleniumInstall(installOpts);
+        } catch (err) {
+          logError({err, plugin: '[selenium install]'});
+        }
+
+        let seleniumStart = thunk(selenium.start, selenium);
+        let child;
+
+        try {
+          child = yield seleniumStart(spawnOpts);
+        } catch (err) {
+          logError({err, plugin: `[selenium start]: ${err.message} => pkill java`});
+        }
+
+        let cp = thunk(runWebdriver);
+        let code = yield cp();
+
+        try {
+          child.kill();
+        } catch (err) {
+          logError({err, plugin: '[selenium: local server kill]'});
+        }
+        logStatus(code);
+      });
+    } else if (testEnv === 'ci') {
+      run(function *() {
+        let cp = thunk(runWebdriver);
+        let code = yield cp();
+
+        logStatus(code);
+      });
+    } else {
+      logError({err: new Error('Your test environment was not defined'), plugin: '[selenium]'});
+    }
+  };
+}
