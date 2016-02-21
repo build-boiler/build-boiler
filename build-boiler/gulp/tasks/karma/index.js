28c9576a56684c4822070ab69325333791e710f9
dtothefp <dtothefp@gmail.com>
2016-02-21 17:51:37 -0500

    add testing to build-boiler
    add copy task for test configs
    
    add selenium runner
    
    update postinstall runner
    
    stop clobbering
    
    update postinstall path
    
    update binary path for webpdriver
    
    add cache to travis
    
    update test path...again
    
    add the root path
    
    finally fix copy path
    
    more path fixes
    
    remove prepublish script
    
    fixup build
    
    fix github url
    
    hopefully finally correct
    
    use root path
    
    add missing dep
    
    update binary path for webdriver
    
    update selenium babel config
    
    add plus tag
    
    fix publish regex
    
    uptate publish script
    
    update publish regex
    
    bump

---
+++ build-boiler/gulp/tasks/karma/index.js
@@ -0,0 +1,84 @@
+import {Server} from 'karma';
+import makeConfig from './karma-config';
+import makeBadge from './coverage-badge';
+
+export default function(gulp, plugins, config) {
+  const {browserSync} = plugins;
+  const {coverage, environment, sources, utils} = config;
+  const {devPort, coverageDir} = sources;
+  const {addbase, logError} = utils;
+  const {isDev} = environment;
+  const serverName = 'test_server';
+
+  return (cb) => {
+    /**
+     * construct the karma config
+     */
+    const karmaConfig = makeConfig(config);
+    const server = new Server(karmaConfig, (code) => {
+      if (cb) {
+        /**
+         * call the gulp `cb` only once
+         */
+        const gulpCb = cb;
+        cb = null;
+
+        if (coverage) {
+
+          /**
+           * if there is a browser-sync server running displaying code coverage kill it
+           */
+          if (isDev) {
+            const bs = browserSync.get(serverName);
+            bs.exit();
+          } else {
+            makeBadge(config).catch((err) => {
+              logError({err, plugin: '[karma: coverage badge]'});
+            });
+          }
+        }
+
+        //hack for karma exit issues https://github.com/karma-runner/karma/issues/1035
+        process.exit(code);
+        gulpCb();
+      }
+    });
+
+    /**
+     * Start the server
+     */
+    server.start();
+
+    if (isDev && coverage) {
+      const bs = browserSync.create(serverName);
+      let hasRun = false;
+
+      /**
+       * listen for the first run test complete and start the browser-sync
+       * server to display test coverage
+       */
+      server.on('run_complete', () => {
+        if (!hasRun) {
+          hasRun = true;
+
+          /**
+           * start a browser-sync server to display code coverage
+           * and watch all code coverage files for changes
+           */
+          bs.init({
+            server: {
+              baseDir: addbase(coverageDir, 'lcov-report')
+            },
+            files: [{
+              match: '**/*.{js,css,html}',
+              options: {
+                cwd: coverageDir
+              }
+            }],
+            port: devPort
+          });
+        }
+      });
+    }
+  };
+}
