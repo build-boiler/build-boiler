159870d58442bbe6b67dcc786cb0d16f56a5f096
dtothefp <dtothefp@gmail.com>
2016-02-21 17:51:37 -0500

    add testing to build-boiler
    add copy task for test configs
    
    add selenium runner
    
    update postinstall runner
    
    stop clobbering
    
    update postinstall path
    
    update binary path for webpdriver
    
    add cache to travis
    
    update test path...again
    
    add the root path
    
    finally fix copy path
    
    more path fixes
    
    remove prepublish script
    
    fixup build
    
    fix github url
    
    hopefully finally correct
    
    use root path
    
    add missing dep
    
    update binary path for webdriver
    
    update selenium babel config
    
    add plus tag
    
    fix publish regex
    
    uptate publish script
    
    update publish regex
    
    bump

--- build-boiler/gulp/tasks/webpack/make-webpack-config.js
+++ build-boiler/gulp/tasks/webpack/make-webpack-config.js
@@ -41,6 +41,7 @@ export default function(config) {
 
   const {
     isServer,
+    babelQuery,
     externals,
     preLoaders,
     loaders,
@@ -105,6 +106,13 @@ export default function(config) {
   };
 
   const commons = {vendors};
+  const coverageConfig = {
+    isparta: {
+      embedSource: true,
+      noAutoWrap: true,
+      babel: babelQuery
+    }
+  };
 
   const configFn = {
     development(isProd) {
@@ -253,6 +261,44 @@ export default function(config) {
       };
 
       return _.merge({}, defaultConfig, serverConfig);
+    },
+
+    test() {
+      const testConfig = {
+        module: {
+          preLoaders,
+          loaders,
+          postLoaders
+        },
+        plugins,
+        watch: true,
+        devtool: 'inline-source-map'
+      };
+
+      return _.merge({}, defaultConfig, testConfig, coverageConfig);
+    },
+
+    ci() {
+      const uglifyLoader = {
+        test: /\.jsx?$/,
+        loader: 'uglify',
+        exclude: /\-spec\.js$/
+      };
+      const ciConfig = {
+        module: {
+          preLoaders,
+          loaders,
+          postLoaders: [uglifyLoader, ...postLoaders]
+        },
+        plugins,
+        // allow getting rid of the UglifyJsPlugin
+        // https://github.com/webpack/webpack/issues/1079
+        'uglify-loader': {
+          compress: {warnings: false}
+        }
+      };
+
+      return _.merge({}, defaultConfig, ciConfig, coverageConfig);
     }
   };
 
