8833f96cb8dd3b2ab028e10899a3280a84e399f0
dtothefp <dtothefp@gmail.com>
2016-02-21 17:51:37 -0500

    add testing to build-boiler
    add copy task for test configs
    
    add selenium runner
    
    update postinstall runner
    
    stop clobbering
    
    update postinstall path
    
    update binary path for webpdriver
    
    add cache to travis
    
    update test path...again
    
    add the root path
    
    finally fix copy path
    
    more path fixes
    
    remove prepublish script
    
    fixup build
    
    fix github url
    
    hopefully finally correct
    
    use root path
    
    add missing dep
    
    update binary path for webdriver
    
    update selenium babel config
    
    add plus tag
    
    fix publish regex
    
    uptate publish script
    
    update publish regex
    
    bump

---
+++ build-boiler/gulp/tasks/karma/browser-stack/browsers.js
@@ -0,0 +1,31 @@
+export default {
+  bs_firefox_mac: {
+    base: 'BrowserStack',
+    browser: 'firefox',
+    browser_version: '43.0',
+    os: 'OS X',
+    os_version: 'Yosemite'
+  },
+  bs_ie: {
+    base: 'BrowserStack',
+    browser: 'ie',
+    browser_version: '11.0',
+    os: 'Windows',
+    os_version: '8.1'
+  },
+  bs_chrome_windows: {
+    base: 'BrowserStack',
+    browser: 'chrome',
+    browser_version: '46.0',
+    os: 'WINDOWS',
+    os_version: '8.1'
+  },
+  bs_safari: {
+    base: 'BrowserStack',
+    browser: 'safari',
+    browser_version: '8.0',
+    os: 'OS X',
+    os_version: 'Yosemite'
+  }
+};
+
