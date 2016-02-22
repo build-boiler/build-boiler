1e9fd0a4b96c32f2a69315baf3eedc1b09f3ed03
dtothefp <dtothefp@gmail.com>
2016-02-22 13:22:35 -0500

    add karma and coverage

--- build-boiler/gulp/tasks/webpack/loaders/index.js
+++ build-boiler/gulp/tasks/webpack/loaders/index.js
@@ -4,12 +4,14 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default function(opts) {
   const {
+    coverage,
     environment,
     sources,
     toolsPlugin,
     utils,
     quick,
     isMainTask,
+    karma,
     webpackConfig,
     DEBUG,
     SERVER,
@@ -33,6 +35,8 @@ export default function(opts) {
   const {fileLoader} = paths;
   const {addbase, addroot} = utils;
   const excludeRe = /^.+\/node_modules\/(?!@hfa\/).+\.jsx?$/;
+  const testCoverage = coverage && TEST;
+  const {coverageRe} = karma;
   const babelQuery = {};
   const babelBaseConfig = _.omit(babelrc, ['env']);
   const imageLoader = 'img?' + [
@@ -221,7 +225,15 @@ export default function(opts) {
     {
       test: /\.jsx?$/,
       exclude(fp) {
-        return excludeRe.test(fp) && fp.indexOf(rootDir) === -1;
+        let ex = false;
+
+        if (testCoverage && !/\@hfa/.test(fp) && !/node_modules/.test(fp)) {
+          ex = coverageRe.test(fp);
+        } else {
+          ex = excludeRe.test(fp) && fp.indexOf(rootDir) === -1;
+        }
+
+        return ex;
       },
       loader: 'babel',
       query: babelRootQuery
@@ -254,6 +266,17 @@ export default function(opts) {
       exclude: excludeRe,
       loader: join(__dirname, 'mocks-loader')
     });
+  }
+
+  const isparta = {
+    test: /\.jsx?$/,
+    loader: 'isparta',
+    exclude: /\/(test|node_modules)\//,
+    include: coverageRe
+  };
+
+  if (testCoverage) {
+    preLoaders.unshift(isparta);
   }
 
   const postLoaders = [];
