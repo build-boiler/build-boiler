2dd1219bc2aa5f1ccf6934eb1c74ef876a5df74a
dtothefp <dtothefp@gmail.com>
2016-02-22 13:22:35 -0500

    add karma and coverage

--- gulp/config/index.js
+++ gulp/config/index.js
@@ -68,6 +68,15 @@ export default {
     ],
     bootstrap: path.join('lib', 'bootstrap')
   },
+  karma: {
+    browsers: {},
+    devices: {},
+    coverageRe: /^.+?\/src\/js\/(?:services|modules|component-utils|module-utils)\/.+?\.jsx?$/
+  },
+  webdriver: {
+    browsers: [],
+    devices: []
+  },
   cb(config) {
     //you have access to the gulp config here for
     //any extra customization => don't forget to return config
