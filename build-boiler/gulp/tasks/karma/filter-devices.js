9c74393ae7ae95d1b6a822f8be4f4d9a97661140
dtothefp <dtothefp@gmail.com>
2016-02-22 13:22:35 -0500

    add karma and coverage

--- build-boiler/gulp/tasks/karma/filter-devices.js
+++ build-boiler/gulp/tasks/karma/filter-devices.js
@@ -1,53 +1,53 @@
 import _ from 'lodash';
-import bsBrowsers from './browser-stack/browsers';
-import bsDevices from './browser-stack/devices.js';
 
-/**
- * Compares CLI desired devices to the configs in `/browser-stack` directory
- * @param {Array} desiredDevices
- * @return {Array} found devices based on CLI config
- */
-export function filterKeys(desiredDevices) {
-  const allBsDevices = _.assign({}, bsBrowsers, bsDevices);
+export default function({bsBrowsers, bsDevices}) {
+  /**
+   * Compares CLI desired devices to the configs in `/browser-stack` directory
+   * @param {Array} desiredDevices
+   * @return {Array} found devices based on CLI config
+   */
+  function filterKeys(desiredDevices) {
+    const allBsDevices = _.assign({}, bsBrowsers, bsDevices);
 
-  return desiredDevices.reduce((list, cliDevice) => {
-    const foundDevice = Object.keys(allBsDevices).filter(bsDevice => {
-      const {browser, device} = allBsDevices[bsDevice];
-      const compare = browser || device;
+    return desiredDevices.reduce((list, cliDevice) => {
+      const foundDevice = Object.keys(allBsDevices).filter(bsDevice => {
+        const {browser, device} = allBsDevices[bsDevice];
+        const compare = browser || device;
 
-      return compare.toLowerCase().indexOf(cliDevice) !== -1;
-    });
+        return compare.toLowerCase().indexOf(cliDevice) !== -1;
+      });
 
-    return [...list, ...foundDevice];
-  }, []);
-}
-
-/**
- * Filter CLI args for BrowserSync browser/devices
- * @param {Object} opts
- * @param {Array|Boolean} desktop
- * @param {Array|Boolean} mobile
- * @return {Array}
- */
-export default function({desktop, mobile}) {
-  const nothingSpecified = _.isUndefined(desktop) && _.isUndefined(mobile);
-  const browserKeys = Object.keys(bsBrowsers);
-  const deviceKeys = Object.keys(bsDevices);
-  let browsers = [];
-  let devices = [];
-
-  if (desktop) {
-    browsers = _.isArray(desktop) ? filterKeys(desktop) : browserKeys;
+      return [...list, ...foundDevice];
+    }, []);
   }
 
-  if (mobile) {
-    devices = _.isArray(mobile) ? filterKeys(mobile) : deviceKeys;
-  }
+  /**
+   * Filter CLI args for BrowserSync browser/devices
+   * @param {Object} opts
+   * @param {Array|Boolean} desktop
+   * @param {Array|Boolean} mobile
+   * @return {Array}
+   */
+  return ({desktop, mobile}) => {
+    const nothingSpecified = _.isUndefined(desktop) && _.isUndefined(mobile);
+    const browserKeys = Object.keys(bsBrowsers);
+    const deviceKeys = Object.keys(bsDevices);
+    let browsers = [];
+    let devices = [];
 
-  if (nothingSpecified) {
-    browsers = browserKeys;
-    devices = deviceKeys;
-  }
+    if (desktop) {
+      browsers = _.isArray(desktop) ? filterKeys(desktop) : browserKeys;
+    }
 
-  return [...browsers, ...devices];
+    if (mobile) {
+      devices = _.isArray(mobile) ? filterKeys(mobile) : deviceKeys;
+    }
+
+    if (nothingSpecified) {
+      browsers = browserKeys;
+      devices = deviceKeys;
+    }
+
+    return [...browsers, ...devices];
+  }
 }
