e257d650c5348b9b817a0c14b5e2060ed0cee635
dtothefp <dtothefp@gmail.com>
2016-03-14 23:08:57 -0400

    update promisify util to pass context

--- packages/boiler-utils/src/cb-to-prom.js
+++ packages/boiler-utils/src/cb-to-prom.js
@@ -2,14 +2,15 @@
  * "Thunk" a node style function with last argument
  * of callback to make use in `async` functions
  * @param {Function} fn
+ * @param {Function|Object} ctx context
  * @return {Function}
  */
-export default function(fn) {
+export default function(fn, {ctx}) {
   return (...args) => {
     return new Promise((res, rej) => {
       const cb = (err, data) => err ? rej(err) : res(data);
 
-      fn.apply(fn, [...args, cb]);
+      fn.apply(ctx || fn, [...args, cb]);
     });
   };
 }
