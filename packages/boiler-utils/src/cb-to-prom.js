b98e54b70e95a4da2ce8a7ac1274cab5763ded43
dtothefp <dtothefp@gmail.com>
2016-03-15 10:45:36 -0400

    update promisify options

--- packages/boiler-utils/src/cb-to-prom.js
+++ packages/boiler-utils/src/cb-to-prom.js
@@ -2,10 +2,13 @@
  * "Thunk" a node style function with last argument
  * of callback to make use in `async` functions
  * @param {Function} fn
- * @param {Function|Object} ctx context
+ * @param {Object} opts options
+ * @param {Function|Object} opts.ctx context
  * @return {Function}
  */
-export default function(fn, {ctx}) {
+export default function(fn, opts = {}) {
+  const {ctx} = opts;
+
   return (...args) => {
     return new Promise((res, rej) => {
       const cb = (err, data) => err ? rej(err) : res(data);
