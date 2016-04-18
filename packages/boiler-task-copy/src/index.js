1a32706d1a31dcb0eee0a3b77ca5ad9ed63d5111
dtothefp <dtothefp@gmail.com>
2016-04-18 17:24:38 -0400

    add copy task

--- packages/boiler-task-copy/src/index.js
+++ packages/boiler-task-copy/src/index.js
@@ -1,36 +1,70 @@
 import boilerUtils from 'boiler-utils';
+import isPlainObject from 'lodash/isPlainObject';
+import es from 'event-stream';
 
 export default function(gulp, plugins, config) {
-  const {sources, utils} = config;
-  const {buildDir, srcDir} = sources;
-  const {rename} = plugins;
-  const {addbase} = utils;
   const {
+    callAndReturn: initParentFn,
     runParentFn: callParent,
-    runCustomTask: runFn
+    runCustomTask: runFn,
+    transformArray
   } = boilerUtils;
-
+  const {
+    copy: parentConfig,
+    sources,
+    utils
+  } = config;
+  const {buildDir, srcDir} = sources;
+  const {addbase} = utils;
   const src = [
     addbase(srcDir, 'img/favicon.ico')
   ];
+  const dest = addbase(buildDir);
 
   return () => {
-    const parentConfig = callParent(arguments, {src});
+    const baseConfig = {
+      src,
+      endpoints: [{
+        src,
+        dest
+      }]
+    };
+
+    const callConfigFn = initParentFn(config);
+    /**
+     * Get config from `gulp/config/index.js
+     */
+    const copyConfig = callConfigFn(parentConfig, baseConfig);
+
+    /**
+     * Call the parent gulp task if one exists
+     */
     const {
       src: newSrc,
+      data,
       fn
-    } = parentConfig;
+    } = callParent(arguments, {src, data: copyConfig});
+    const {endpoints} = data || copyConfig;
+
+    function makeTask(src, dest) {
+      return gulp.src(src)
+        .pipe(gulp.dest(dest));
+    }
 
     const task = () => {
-      return gulp.src(newSrc, {base: srcDir})
-        .pipe(rename((fp) => {
-          const {basename} = fp;
+      let tasks;
 
-          if (basename === 'favicon') {
-            fp.dirname = '';
-          }
-        }))
-        .pipe(gulp.dest(buildDir));
+      if (newSrc && src !== newSrc) {
+        tasks = makeTask(newSrc, dest);
+      } else {
+        tasks = transformArray(endpoints, isPlainObject).map(data => {
+          const {src, dest} = data;
+
+          return makeTask(src, dest);
+        });
+      }
+
+      return es.merge(tasks);
     };
 
     return runFn(task, fn);
