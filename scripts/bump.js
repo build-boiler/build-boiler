59bff63f29de3e94881b54464e018094bb971396
dtothefp <dtothefp@gmail.com>
2016-05-12 15:27:13 -0400

    add bump script

---
+++ scripts/bump.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const glob = require('globby').sync;
+const write = require('fs').writeFileSync;
+
+const {argv} = process;
+const versionI = argv.indexOf('-v');
+const force = argv.includes('--force');
+let version;
+
+if (versionI > -1) {
+  ([version] = argv.slice(versionI + 1, versionI + 2));
+} else {
+  const {version: pkgVersion} = require(
+    path.join(process.cwd(), 'lerna.json')
+  );
+  version = Math.floor(pkgVersion) + 1;
+}
+
+if (!version) throw new Error('No version specified');
+if (version.split('.').length !== 3) throw new Error('Invalid semver version');
+
+const pkgs = glob('packages/*/package.json');
+
+pkgs.forEach((fp) => {
+  const json = require(
+    path.join(process.cwd(), fp)
+  );
+
+  if (parseFloat(json.version) < parseFloat(version) || force) {
+    const content = JSON.stringify(
+      Object.assign({}, json, {version}),
+      null,
+      '  '
+    );
+    write(fp, content);
+  }
+});
