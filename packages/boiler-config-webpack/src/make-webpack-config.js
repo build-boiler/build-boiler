2f4a6e9144780411e0092116d29b6532571e99d7
dtothefp <dtothefp@gmail.com>
2016-10-19 11:47:14 -0400

    updates for webpack hashing and subresource integrity

--- packages/boiler-config-webpack/src/make-webpack-config.js
+++ packages/boiler-config-webpack/src/make-webpack-config.js
@@ -5,6 +5,7 @@ import isPlainObject from 'lodash/isPlainObject';
 import omit from 'lodash/omit';
 import {join} from 'path';
 import webpack from 'webpack';
+import WebpackMd5Hash from 'webpack-md5-hash';
 import formatter from 'eslint-friendly-formatter';
 import boilerUtils from 'boiler-utils';
 import getLoaderPluginConfig from './get-loader-plugin-config';
@@ -211,6 +212,10 @@ export default function(config, defaultConfig, opts = {}) {
 
     return prodConfig;
   };
+
+  prodConfig.plugins.push(
+    new WebpackMd5Hash()
+  );
 
   return callParent(methodCb, {
     development,
